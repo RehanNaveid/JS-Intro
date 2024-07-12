ddfcdd97d6c0490ac2d2eec444cb8d697fba3769
Rehan Naveid <119671994+RehanNaveid@users.noreply.github.com>
2024-07-13 00:29:20 +0530

    Update control flow.js

--- Intro to Js/control flow.js
+++ Intro to Js/control flow.js
@@ -1,50 +1,50 @@
 function checkAge(age){
     if (age>=18){
-        console.log("Adult")
+        console.log("Adult");
     }
     else{
-        console.log("Minor")
+        console.log("Minor");
     }
 }
 
-checkAge(20)
+checkAge(20);
 
 function printNumbers(n){
     for (i=0;i<=n;i++){
-        console.log(i)
+        console.log(i);
     }
 }
 
-printNumbers(10)
+printNumbers(10);
 
 function countdown(start){
-    let i=start
+    let i=start;
     while(i>0){
-        console.log(i)
-        i--
+        console.log(i);
+        i--;
     }
 }
 
-countdown(5)
+countdown(5);
 
 function printOddNumbers(n){
     for (i=0;i<=n;i++){
         if(i%2==0){
-            continue
+            continue;
         }
-        console.log(i)
+        console.log(i);
     }
 }
 
-printOddNumbers(20)
+printOddNumbers(20);
 
 function findFirstMultiple(n,multiple){
     for(i=1;i<=n;i++){
         if (i%multiple==0){
-            return i
-            break
+            return i;
+            break;
         }
     }
 }
 
-console.log(findFirstMultiple(50,2))
+console.log(findFirstMultiple(50,2));
